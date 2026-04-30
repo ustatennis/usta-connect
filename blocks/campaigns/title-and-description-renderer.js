d2edf4c8ec310ec8eef41d17a46e31e6a6040322
pothusaitheja <sai.theja@contractor.usta.com>
2026-04-30 12:26:27 -0400

    lint fix

--- blocks/campaigns/title-and-description-renderer.js
+++ blocks/campaigns/title-and-description-renderer.js
@@ -4,7 +4,9 @@ export default class titleAndDescriptionRenderer {
     this.eGui.innerHTML = `<div class="cellicon">
         <div class="cellheader">${
           params.data?.Title || params.data?.Name
-        }</div><div class="marketing-trigger-number">${params.data.Name || ''}</div>
+        }</div><div class="marketing-trigger-number">${
+      params.data.Name || ''
+    }</div>
         <div class="cellsubheader">${params.data.Description || ''}</div>
         </div>`;
   }
